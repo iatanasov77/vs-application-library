08551275c5ab4c0be01bad338e429c1be015f413
Ivan I. Atanasov <i.atanasov77@gmail.com>
2020-09-18 08:46:43 +0000

    Add More fixes about merge from master.

---
+++ lib/Controller/SettingsController.php
@@ -0,0 +1,8 @@
+<?php namespace VS\ApplicationBundle\Controller;
+
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+
+class SettingsController extends ResourceController
+{
+    
+}

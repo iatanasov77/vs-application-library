aca22095ab9acaae1c94c076d9f83be34e4a72e1
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-06-11 17:12:29 +0000

    Fix ClassName Colision.

--- lib/Repository/TaxonRepository.php
+++ lib/Repository/TaxonRepository.php
@@ -1,11 +1,11 @@
 <?php namespace VS\ApplicationBundle\Repository;
 
-use Sylius\Bundle\TaxonomyBundle\Doctrine\ORM\TaxonRepository;
+use Sylius\Bundle\TaxonomyBundle\Doctrine\ORM\TaxonRepository as BaseTaxonRepository;
 use Doctrine\ORM\Query\ResultSetMappingBuilder;
 
 // use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
 // class TaxonRepository extends NestedTreeRepository
-class TaxonRepository extends TaxonRepository
+class TaxonRepository extends BaseTaxonRepository
 {
     public function getTaxonsAsArray( $rootTaxonId, $parentId )
     {

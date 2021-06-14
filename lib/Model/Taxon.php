9b94440eaddcef2d35455c651934f3b89363e1d6
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-06-14 11:50:01 +0000

    Fix Taxon ORM Mapping.

--- lib/Model/Taxon.php
+++ lib/Model/Taxon.php
@@ -1,34 +1,26 @@
 <?php namespace VS\ApplicationBundle\Model;
 
-use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
-
 use Doctrine\Common\Collections\ArrayCollection;
 use Sylius\Component\Taxonomy\Model\Taxon as BaseTaxon;
-use Sylius\Component\Resource\Model\TranslationInterface;
 use Sylius\Component\Taxonomy\Model\TaxonInterface;
-use Sylius\Component\Taxonomy\Model\TaxonTranslationInterface;
 
 use VS\ApplicationBundle\Model\Interfaces\TaxonInterface as VsTaxonInterface;
 
 /**
  * XML Mapping don't working
  * 
- * @ORM\MappedSuperclass
+ * ___@ORM\MappedSuperclass
  */
 class Taxon extends BaseTaxon implements VsTaxonInterface
 {
-    /**
-     * @ORM\OneToOne(targetEntity="VS\ApplicationBundle\Model\Interfaces\TaxonomyInterface", mappedBy="rootTaxon")
-     */
     protected $taxonomy;
     
-    public function hasChild(TaxonInterface $taxon): bool
+    public function hasChild( TaxonInterface $taxon ): bool
     {
         if ( ! $this->children ) {
             $this->children = new ArrayCollection();
         }
-        return $this->children->contains($taxon);
+        return $this->children->contains( $taxon );
     }
     
     public function getCurrentLocale()

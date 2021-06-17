7ea42f99bdd31bd32de2631215a002738a3d6f8c
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-06-17 13:16:58 +0000

    Fix SyntaxError on Migration for Taxonomy Table.

--- lib/DoctrineMigrations/Version20210617123114.php
+++ lib/DoctrineMigrations/Version20210617123114.php
@@ -16,7 +16,7 @@ final class Version20210617123114 extends AbstractMigration
     {
         return '';
     }
-
+    
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
@@ -29,10 +29,10 @@ final class Version20210617123114 extends AbstractMigration
         $this->addSql('ALTER TABLE VSAPP_Settings ADD CONSTRAINT FK_4A491FD762596F6 FOREIGN KEY (site_id ) REFERENCES VSAPP_Sites (id)');
         $this->addSql('CREATE INDEX IDX_4A491FD507FAB6A ON VSAPP_Settings (maintenance_page_id )');
         $this->addSql('CREATE INDEX IDX_4A491FD762596F6 ON VSAPP_Settings (site_id )');
-        $this->addSql('ALTER TABLE VSAPP_Taxonomy ADD `code` VARCHAR(255) NOT NULL AFTER `id`');
+        $this->addSql('ALTER TABLE VSAPP_Taxonomy ADD code VARCHAR(255) NOT NULL AFTER id');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_1CF3890577153098 ON VSAPP_Taxonomy (code)');
     }
-
+    
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
@@ -46,6 +46,6 @@ final class Version20210617123114 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_4A491FD762596F6 ON VSAPP_Settings (site_id)');
         $this->addSql('CREATE INDEX IDX_4A491FD507FAB6A ON VSAPP_Settings (maintenance_page_id)');
         $this->addSql('DROP INDEX uniq_1cf3890577153098 ON VSAPP_Taxonomy');
-        $this->addSql('ALTER TABLE VSAPP_Taxonomy DROP `code`);;
+        $this->addSql('ALTER TABLE VSAPP_Taxonomy DROP code');
     }
 }

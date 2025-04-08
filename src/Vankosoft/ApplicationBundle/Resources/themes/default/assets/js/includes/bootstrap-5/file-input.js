576f1a8e583a02d27d305fed61b2ec1907562cad
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-04-08 08:09:11 +0000

    Improve Bootstrap5 'file_input.js' .

--- src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/includes/bootstrap-5/file-input.js
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/includes/bootstrap-5/file-input.js
@@ -2,14 +2,14 @@ import './file-input.css';
 
 $( function()
 {
-    $( 'div.form-field-file input[type=file]' ).on( 'change', function()
+    $( document ).on( 'change', 'div.form-field-file input[type=file]', function()
     {
         var label       = $( this ).next();
         var fileName    = $( this ).val().split( '\\' ).pop();
         if ( fileName ) { 
             $( label ).html( fileName );
         } else { 
-            $( label ).html( '' );
+            $( label ).html( 'Choose File' );
         }
     });
 });

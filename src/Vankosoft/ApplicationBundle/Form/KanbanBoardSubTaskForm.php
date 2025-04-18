cde436b21fed316ab114d71eb40b35c141c58ecd
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-04-18 13:00:59 +0000

    Create KanbanBoard Task Sub-Tasks.

--- src/Vankosoft/ApplicationBundle/Form/KanbanBoardSubTaskForm.php
+++ src/Vankosoft/ApplicationBundle/Form/KanbanBoardSubTaskForm.php
@@ -4,6 +4,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 

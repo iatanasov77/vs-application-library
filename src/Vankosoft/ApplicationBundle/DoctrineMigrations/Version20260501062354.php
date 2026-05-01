fc6cba17cce7420cf599572f5492a3aae279a7c4
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-05-01 06:28:36 +0000

    Add a Doctrine Migration.

---
+++ src/Vankosoft/ApplicationBundle/DoctrineMigrations/Version20260501062354.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vankosoft\ApplicationBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260501062354 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE VSCMS_QuickLinks ADD link_icon_path VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE VSUM_UsersInfo CHANGE title title ENUM(\'mr\', \'mrs\', \'miss\')');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE VSCMS_QuickLinks DROP link_icon_path');
+        $this->addSql('ALTER TABLE VSUM_UsersInfo CHANGE title title VARCHAR(255) DEFAULT NULL');
+    }
+}

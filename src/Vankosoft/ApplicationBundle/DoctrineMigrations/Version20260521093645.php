d6f9e5e4c84a4bb0ca07c8f55042ca866d479666
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-05-21 09:43:39 +0000

    Add Last Activity Property in User Model.

---
+++ src/Vankosoft/ApplicationBundle/DoctrineMigrations/Version20260521093645.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vankosoft\ApplicationBundle\DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260521093645 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE VSUM_Users ADD last_activity_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE VSUM_UsersInfo CHANGE title title ENUM(\'mr\', \'mrs\', \'miss\')');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE VSUM_Users DROP last_activity_at');
+        $this->addSql('ALTER TABLE VSUM_UsersInfo CHANGE title title VARCHAR(255) DEFAULT NULL');
+    }
+}

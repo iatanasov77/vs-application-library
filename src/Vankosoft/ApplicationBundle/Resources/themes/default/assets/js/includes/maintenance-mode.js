c9c4945949224f823665b3686b370ecce3afa995
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-05-25 04:35:54 +0000

    Create JS Helpers for Bootstrap Alerts and Maintenance Mode.

--- src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/includes/maintenance-mode.js
+++ src/Vankosoft/ApplicationBundle/Resources/themes/default/assets/js/includes/maintenance-mode.js
@@ -7,12 +7,10 @@ export function CheckMaintenanceMode( url )
         url: url,        
         success: function( data, textStatus, xhr ) {
             console.log( xhr.status );
-            alert( xhr.status );
             window.MaintenanceModeStatus = xhr.status;
         },
         complete: function( xhr, textStatus ) {
             console.log( xhr.status );
-            alert( xhr.status );
             window.MaintenanceModeStatus = xhr.status;
         } 
     });

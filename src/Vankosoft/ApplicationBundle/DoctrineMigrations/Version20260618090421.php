3faae93e9915597820a17b4dfb73e221b5c93cd6
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-06-18 09:32:20 +0000

    Make Quick Links in Category Reordable.

--- src/Vankosoft/ApplicationBundle/DoctrineMigrations/Version20260618090421.php
+++ src/Vankosoft/ApplicationBundle/DoctrineMigrations/Version20260618090421.php
@@ -21,15 +21,11 @@ final class Version20260618090421 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE VSCMS_QuickLinks ADD priority INT DEFAULT 0 NOT NULL');
-        $this->addSql('ALTER TABLE VSCMS_SlidersItemsPhotos ADD CONSTRAINT FK_A9581A697E3C61F9 FOREIGN KEY (owner_id) REFERENCES VSCMS_SlidersItems (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE VSUM_UsersInfo CHANGE title title ENUM(\'mr\', \'mrs\', \'miss\')');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE VSCMS_QuickLinks DROP priority');
-        $this->addSql('ALTER TABLE VSCMS_SlidersItemsPhotos DROP FOREIGN KEY FK_A9581A697E3C61F9');
-        $this->addSql('ALTER TABLE VSUM_UsersInfo CHANGE title title VARCHAR(255) DEFAULT NULL');
     }
 }
